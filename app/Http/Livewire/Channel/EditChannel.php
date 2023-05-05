77662bd68a80f0dd3bcad41bd1cb800aeb24bd38
naelahmad <nael.a.20171@gmail.com>
2023-05-05 11:54:16 +0300

    feat: Configure Validation via Livewire And Make The Edit Form Work

--- app/Http/Livewire/Channel/EditChannel.php
+++ app/Http/Livewire/Channel/EditChannel.php
@@ -8,6 +8,15 @@ use Livewire\Component;
 class EditChannel extends Component
 {
     public $channel;
+
+    protected function rules()
+    {
+        return [
+            'channel.name' => 'required|max:255|min:4|unique:channels,name,' . $this->channel->id,
+            'channel.slug' => 'required|max:255|unique:channels,slug,' . $this->channel->id,
+            'channel.description' => 'nullable|max:1000',
+        ];
+    }
     public function mount(Channel $channel)
     {
         $this->channel = $channel;
@@ -15,5 +24,22 @@ class EditChannel extends Component
     public function render()
     {
         return view('livewire.channel.edit-channel');
+
+    }
+    public function update()
+    {
+        $this->validate();
+
+        $this->channel->update([
+            'name' => $this->channel->name,
+            'slug' => $this->channel->slug,
+            'description' => $this->channel->description,
+        ]);
+
+        session()->flash('message', 'Channel successfully updated.');
+        return redirect()->route('channel.edit', ['channel' => $this->channel->slug]);
+
+
+
     }
 }
--- resources/views/livewire/channel/edit-channel.blade.php
+++ resources/views/livewire/channel/edit-channel.blade.php
@@ -1,17 +1,44 @@
 <div>
+    @if (session()->has('message'))
+        <div class="alert alert-success">
+            {{ session('message') }}
+        </div>
+    @endif
     <form wire:submit.prevent="update">
         <div class="form-group">
             <label for="name">Name</label>
-            <input type="text" class="form-control">
+            <input type="text" class="form-control @error('channel.name') is-invalid @enderror"
+                wire:model="channel.name">
+            @error('channel.name')
+                <span class="invalid-feedback" role="alert">
+                    <strong>{{ $message }}</strong>
+                </span>
+            @enderror
         </div>
+
+
         <div class="form-group">
             <label for="slug">Slug</label>
-            <input type="text" class="form-control">
+            <input type="text" class="form-control @error('channel.slug') is-invalid @enderror"
+                wire:model="channel.slug">
+            @error('channel.slug')
+                <span class="invalid-feedback" role="alert">
+                    <strong>{{ $message }}</strong>
+                </span>
+            @enderror
         </div>
+
         <div class="form-group">
             <label for="description">Description</label>
-            <textarea cols="40" rows="4" class="form-control"></textarea>
+            <textarea cols="40" rows="4" class="form-control @error('channel.description') is-invalid @enderror"
+                wire:model="channel.description"></textarea>
+            @error('channel.description')
+                <span class="invalid-feedback" role="alert">
+                    <strong>{{ $message }}</strong>
+                </span>
+            @enderror
         </div>
+
         <div class="form-group">
             <button type="submit" class="btn btn-primary">Update</button>
         </div>
