258628cecd9f4ae7ce9bf4ad76b4a0604cb733b8
naelahmad <nael.a.20171@gmail.com>
2023-05-05 11:13:44 +0300

    feat: Build The Edit Form

--- resources/views/livewire/channel/edit-channel.blade.php
+++ resources/views/livewire/channel/edit-channel.blade.php
@@ -1,4 +1,19 @@
 <div>
-    {{-- Be like water. --}}
-    {{ $channel->name }}
+    <form wire:submit.prevent="update">
+        <div class="form-group">
+            <label for="name">Name</label>
+            <input type="text" class="form-control">
+        </div>
+        <div class="form-group">
+            <label for="slug">Slug</label>
+            <input type="text" class="form-control">
+        </div>
+        <div class="form-group">
+            <label for="description">Description</label>
+            <textarea cols="40" rows="4" class="form-control"></textarea>
+        </div>
+        <div class="form-group">
+            <button type="submit" class="btn btn-primary">Update</button>
+        </div>
+    </form>
 </div>
