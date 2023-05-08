9d38cd9ff96ea5e8bef8583b13fbf3b7b39caa3e
naelahmad <nael.a.20171@gmail.com>
2023-05-08 21:33:39 +0300

    Feat: Edit Video Record Form And Writing Code To Uplod Image For Videos

--- app/Http/Livewire/Video/EditVideo.php
+++ app/Http/Livewire/Video/EditVideo.php
@@ -2,12 +2,60 @@
 
 namespace App\Http\Livewire\Video;
 
+use App\Models\Channel;
+use App\Models\Video;
 use Livewire\Component;
+use Livewire\WithFileUploads;
+use Image;
+
 
 class EditVideo extends Component
 {
+    use WithFileUploads;
+
+    public Channel $channel;
+    public Video $video;
+    public $image;
+
+    protected $rules = [
+        'video.title' => 'required|max:255',
+        'video.description' => 'nullable|max:1000',
+        'video.visibility' => 'required|in:private,public,unlisted',
+        'image' => 'nullable|image|mimes:png,jpeg,png,jpg,gif,svg|max:2048',
+    ];
+    public function mount($channel, $video)
+    {
+        $this->channel = $channel;
+        $this->video = $video;
+    }
     public function render()
     {
         return view('livewire.video.edit-video')->extends('layouts.app');
+    }
+    public function update()
+    {
+        #validation
+        $this->validate();
+        if ($this->image) {
+            $image = $this->image->storeAs('videos', $this->channel->uid . '.png');
+            $imageName = explode('/', $image)[1];
+
+            //resize and convert to png
+            $img = Image::make(storage_path() . '/app/' . $image)->encode('png')->fit(250, 250, function ($constraint) {
+                $constraint->upsize();
+            })->save();
+
+            //update file path in db
+            $this->channel->update([
+                'image' => $imageName,
+            ]);
+        }
+        $this->video->update([
+            'title' => $this->video->title,
+            'description' => $this->video->description,
+            'visibility' => $this->video->visibility,
+        ]);
+        session()->flash('message', 'Video successfully updated.');
+        //return redirect()->route('channel.edit', ['channel' => $this->channel->slug]);
     }
 }
--- resources/views/livewire/video/edit-video.blade.php
+++ resources/views/livewire/video/edit-video.blade.php
@@ -1,3 +1,80 @@
 <div>
-    {{-- Success is as dangerous as failure. --}}
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-body">
+
+                        @if (session()->has('message'))
+                            <div class="alert alert-success">
+                                {{ session('message') }}
+                            </div>
+                        @endif
+                        @if ($video->image)
+                            <img src="{{ asset('videos' . '/' . $video->image) }}" class="img-thumbnail">
+                        @endif
+                        <form wire:submit.prevent="update">
+                            <div class="form-group mb-2">
+                                <label for="title">Title</label>
+                                <input type="text" class="form-control @error('video.title') is-invalid @enderror"
+                                    wire:model="video.title">
+                                @error('video.title')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+
+                            <div class="form-group mb-2">
+                                <label for="description">Description</label>
+                                <textarea cols="40" rows="4" class="form-control @error('video.description') is-invalid @enderror"
+                                    wire:model="video.description"></textarea>
+                                @error('video.description')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                            <div class="form-group">
+                                <label for="image">Image</label>
+                                <input type="file" class="form-control @error('image') is-invalid @enderror"
+                                    wire:model="image" />
+                                <div class="form-group">
+                                    <div wire:loading wire:target="image">Uploading...</div>
+                                    @if ($image)
+                                        Photo Preview:
+                                        <img src="{{ $image->temporaryUrl() }}" class="img-thumbnail">
+                                    @endif
+                                </div>
+
+
+                                @error('image')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                            <div class="form-group mb-2">
+                                <label for="visibility">Visibility</label>
+                                <select wire:model="video.visibility">
+                                    <option value="private">Private</option>
+                                    <option value="public">Public</option>
+                                    <option value="unlisted">Unlisted</option>
+                                </select>
+                                @error('video.visibility')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+
+                            <div class="form-group">
+                                <button type="submit" class="btn btn-primary">Update</button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
